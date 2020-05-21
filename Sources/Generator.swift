5bacf3691dd77268d7f683e8fe382dc949614e4b
banjun <banjun@gmail.com>
2020-05-22 03:25:39 +0900

    pass test with Upgrading drafter 3.2.7 -> 5.0.0

--- Sources/Generator.swift
+++ Sources/Generator.swift
@@ -97,7 +97,7 @@ extension TransitionElement: SwiftConvertible {
         func allResponses(method: String) -> [HTTPResponseElement] {
             otherTransitions
                 .flatMap {t in t.transactions.map {(transition: t, transaction: $0)}}
-                .filter {$0.transaction.request?.attributes.method.rawValue == method &&
+                .filter {$0.transaction.request?.attributes.method.content.rawValue == method &&
                     resource.href(transition: $0.transition, request: $0.transaction.request!) == href}
                 .flatMap {$0.transaction.responses}
         }
@@ -166,7 +166,7 @@ extension TransitionElement: SwiftConvertible {
     }
 }
 """, environment: stencilEnvironment)
-        let siblingResponses = allResponses(method: request.attributes.method.rawValue)
+        let siblingResponses = allResponses(method: request.attributes.method.content.rawValue)
         let responseCases = try siblingResponses.map { r -> [String: Any] in
             let type: String
             let contentTypeEscaped = (r.attributes.headers?.contentType ?? "").replacingOccurrences(of: "/", with: "_")
@@ -220,7 +220,7 @@ extension TransitionElement: SwiftConvertible {
             "publicMemberwiseInit": `public`,
             "name": requestTypeName,
             "responseCases": responseCases,
-            "method": "." + request.attributes.method.rawValue.lowercased(),
+            "method": "." + request.attributes.method.content.rawValue.lowercased(),
             "path": href
         ]
         if let hrefVariables = attributes?.hrefVariables {
@@ -274,10 +274,10 @@ extension TransitionElement: SwiftConvertible {
 
 
     func swiftRequestTypeName(request: HTTPRequestElement, resource: ResourceElement) throws -> String {
-        if let title = meta?.title, let first = title.first {
+        if let title = meta?.title?.content, let first = title.first {
             return (String(first).uppercased() + String(title.dropFirst())).swiftIdentifierized()
         } else {
-            return (request.attributes.method.rawValue + "_" + resource.href(transition: self, request: request)).swiftIdentifierized()
+            return (request.attributes.method.content.rawValue + "_" + resource.href(transition: self, request: request)).swiftIdentifierized()
         }
     }
 }
@@ -301,7 +301,7 @@ extension MemberElement {
         }
         return name + (required ? "" : "?")
     }
-    var swiftDoc: String {return [meta?.description, content.displayValue.map {" ex. " + $0}]
+    var swiftDoc: String {return [meta?.description?.content, content.displayValue.map {" ex. " + $0}]
         .compactMap {$0}
         .joined(separator: " ")
         .docCommentPrefixed()}
@@ -322,7 +322,8 @@ extension MemberElement {
         return MemberElement(
             element: element,
             meta: meta,
-            attributes: Attributes(typeAttributes: attributes?.typeAttributes?.filter {$0 != "required"}),
+            attributes: Attributes(typeAttributes: attributes?.typeAttributes.map {
+                ArrayElement<StringElement>(element: ArrayElement<StringElement>.elementName, content: $0.content.filter {$0.content != "required"})}),
             content: .init(
                 key: content.key,
                 value: .indirect(exactType)))
